58f8ac73c636cf5f9f2042d7086d07324bc4d6e2
Luis Vallejo <57255860+lvallejomendez@users.noreply.github.com>
2023-03-03 19:13:17 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,12 +7,13 @@ Test cases can be run with the following:
 """
 import os
 import logging
+import requests
 from unittest import TestCase
 from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
-from flask import jsonify, request
+from flask import jsonify
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
